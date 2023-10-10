16c5dbfce090e92077eff63a23d537bea14e2033
Maksim-Kots <112854062+Maksim-Kots@users.noreply.github.com>
2023-10-11 02:16:52 +0500

    Add files via upload

---
+++ calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿    int num1 = 0; int num2 = 0;
+
+    Console.WriteLine("Первое число");
+    num1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Второе число");
+    num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Choose an option from the following list:");
+    Console.WriteLine("\tw - Плюс");
+    Console.WriteLine("\ta - минус");
+    Console.WriteLine("\ts - Умножить");
+    Console.WriteLine("\td - Разделить");
+    Console.Write("Вы выбрали ");
+
+    switch (Console.ReadLine())
+    {
+        case "w":
+            Console.WriteLine($"Результат: {num1} + {num2} = " + (num1 + num2));
+            break;
+        case "a":
+            Console.WriteLine($"Результат: {num1} - {num2} = " + (num1 - num2));
+            break;
+        case "s":
+            Console.WriteLine($"Результат: {num1} * {num2} = " + (num1 * num2));
+            break;
+        case "d":
+            Console.WriteLine($"Результат: {num1} / {num2} = " + (num1 / num2));
+            break;
+    }
+    // Wait for the user to respond before closing.
+    Console.Write("Нажмите кнопку что бы выключить");
+    Console.ReadKey();
